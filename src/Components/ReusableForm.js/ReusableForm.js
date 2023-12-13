435a6203fb7f37c7a2913fe25727e236ddc1122f
Suzanne Schuber <suzanneschuber@gmail.com>
2023-12-12 17:56:39 -0800

    WIP reuseable form or adding or updating a pet.

--- src/Components/ReusableForm.js/ReusableForm.js
+++ src/Components/ReusableForm.js/ReusableForm.js
@@ -1,1 +1,20 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+
+const ReusableForm = ({onSubmit, initialData}) => {
+
+  const [formData, setFormData] = useState(initialData || getDefaultFormData());
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onSubmit(formData);
+  };
+
+  const handleInputChange = (event) => {
+    const {name, value, type, checked} = event.target;
+    setFormData({
+      ...formData,
+      [name]: type === 'checkbox' ? checked : value,
+    });
+  };
+  
+}
