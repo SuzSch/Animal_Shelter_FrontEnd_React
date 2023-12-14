c3aed5f12b65786f2525f9ff27f0746ec903302c
Suzanne Schuber <suzanneschuber@gmail.com>
2023-12-14 11:04:16 -0800

    WIP reusable form, update to conditionally render based on pet type chosen from drop down.

--- src/Components/ReusableForm.js/ReusableForm.js
+++ src/Components/ReusableForm.js/ReusableForm.js
@@ -1,20 +1,49 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 
-const ReusableForm = ({onSubmit, initialData}) => {
+const ReusableForm = ({ onSubmit, initialData }) => {
 
   const [formData, setFormData] = useState(initialData || getDefaultFormData());
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    onSubmit(formData);
-  };
-
   const handleInputChange = (event) => {
-    const {name, value, type, checked} = event.target;
+    const { name, value, type, checked } = event.target;
     setFormData({
       ...formData,
       [name]: type === 'checkbox' ? checked : value,
     });
   };
-  
+
+  const getDefaultFormData = () => ({
+    Name: '',
+    Age: '',
+    Breed: '',
+    CoatColor: '',
+    FivPositive: false,
+    DogSize: '',
+    PetType: 'Cat', //default set to Cat
+  });
+
+  useEffect(() => {
+    setFormData(initialData || getDefaultFormData());
+  }, [initialData]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onSubmit(formData);
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label>
+          Pet Type:
+          <select
+          name="PetType"
+          value={formData.PetType}
+          onChange={handleInputChange}>
+            <option value="Cat">Cat</option>
+            <option value="Dog">Dog</option>
+          </select>
+        </label>
+      </div>
+    </form>
+  )
 }
